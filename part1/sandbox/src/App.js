11431797bfe0e3533c2e724e50112157b4d7bd73
Ville Strengell <villes-97@hotmail.com>
2022-09-14 19:38:05 +0300

    part 1.5->Hookien Säännöt

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -1,17 +1,55 @@
-const App = () => {
+import { useState } from 'react'
 
-  const now = new Date()
-  const a = 10
-  const b = 20
 
-  return (
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  } else (
     <div>
-      <p>Hello World, it is {now.toString()}</p>
-      <p>
-        {a} + {b} = {a+b}
-      </p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-export default App;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+ 
+  return (
+    <div>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text="Left"/>
+        <Button handleClick={handleRightClick} text="Right"/>
+        {right}
+        <History allClicks={allClicks}/>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+export default App
