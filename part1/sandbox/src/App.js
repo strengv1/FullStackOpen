cfa14315e6df153b4784343c412e023bfc27d1f2
Ville Strengell <villes-97@hotmail.com>
2022-09-29 10:28:03 +0300

    1.6 alustus

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -8,16 +8,19 @@ const History = (props) => {
         the app is used by pressing the buttons
       </div>
     )
-  } else (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
+  } else {
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+const Display = props => <div>{props.value}</div>
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
@@ -25,10 +28,16 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    setLeft(left + 1)ls
+    
   }
 
   const handleRightClick = () => {
@@ -44,6 +53,11 @@ const App = () => {
         <Button handleClick={handleRightClick} text="Right"/>
         {right}
         <History allClicks={allClicks}/>
+        <br/><br/>
+        <Display value={value} />
+        <Button handleClick={() => setToValue(1000)} text="thousand" />
+        <Button handleClick={() => setToValue(0)} text="reset" />
+        <Button handleClick={() => setToValue(value + 1)} text="increment" />
       </div>
     </div>
   )
