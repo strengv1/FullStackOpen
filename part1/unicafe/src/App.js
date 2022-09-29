4f5bab86c734643f6a86fd36ebbaf34d5eaccfb2
Ville Strengell <villes-97@hotmail.com>
2022-09-29 11:15:14 +0300

    1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- 
+  const all = good+neutral+bad
+
   const Header = ({text}) => (
     <div class="header">
       <h1>{text}</h1>
@@ -28,6 +29,9 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {(good-bad)/(all)}</div>
+        <div>positive {good/all}</div>
       </div>  
     </>
   )
