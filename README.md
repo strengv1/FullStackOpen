7c8f5b5ae9c3efaf5ef77ae43b50b81a7aa4a60f
Ville Strengell <villes-97@hotmail.com>
2022-11-07 09:36:23 +0200

    refactoring

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Hello :)
 
-Individual implementations for 1.1-2.4 are available in git history. 
-2.5 and forward I made separate projects for each exercise.
+Individual implementations are available in git commits :)
--- part2/2.kurssitiedot/src/App.js
+++ part2/2.kurssitiedot/src/App.js
@@ -1,109 +1,63 @@
 
 
+import Course from './components/Course.js'
 
-const Course = ({ course }) => {
-  const Header = ({ coursename }) => {
-    return  <h2> {coursename} </h2>
-  }
-  const Content = ({ parts }) => {
-    const Part = (props) => {
-      return (
-        <>
-          <p>{props.name + " " + props.exercises} </p>
-        </>
-      )
-    }
-    return (
-      <div>
-        {parts.map(
-          part =>
-            <Part key={part.id} name={part.name} exercises={part.exercises} />
-        )}
-      </div>
-    )
-  }
-  const Total = ({ parts }) => {
-    const sum = parts.reduce(
-      (s, p) => s + p.exercises, 0
-    );
-
-    return (
-      <div>
-        <b>total of {sum} exercises</b>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <Header coursename={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
 const App = () => {
   const courses = [
-    {
-      name: 'Half Stack application development',
+    { name: 'Half Stack application development',
       id: 1,
-      parts: [
-        {
+      parts: [ { 
           name: 'Fundamentals of React',
           exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
+          id: 1 },
+        { name: 'Using props to pass data',
           exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
+          id: 2 },
+        { name: 'State of a component',
           exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
+          id: 3 },
+        { name: 'Redux',
           exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
+          id: 4 } ] }, 
+    { name: 'Node.js',
       id: 2,
-      parts: [
-        {
+      parts: [ {
           name: 'Routing',
           exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
+          id: 1 },
+        { name: 'Middlewares',
           exercises: 7,
-          id: 2
-        }
+          id: 2 }
       ]
     }, 
     {
       name: 'Oma kurssi',
       id: 3,
-      parts: [
-        {
+      parts: [ {
           name: 'Eka osa',
           exercises: 5,
-          id: 1
-        },
-        {
-          name: 'Toka osa',
+          id: 1 },
+        { name: 'Toka osa',
           exercises: 7,
-          id: 2
-        },
-        {
-          name: 'Kolmas osa',
+          id: 2 },
+        { name: 'Kolmas osa',
           exercises: 6,
-          id: 3
-        }
+          id: 3 }
+      ]
+    }, 
+    {
+      name: 'Oma kurssi2',
+      id: 4,
+      parts: [ {
+          name: 'First',
+          exercises: 6,
+          id: 1 },
+        { name: 'second',
+          exercises: 7,
+          id: 2 },
+        { name: 'Third',
+          exercises: 6,
+          id: 3 }
       ]
     }
   ]
