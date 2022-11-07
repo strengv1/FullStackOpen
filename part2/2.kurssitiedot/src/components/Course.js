d1e7902ab182a49a3994dd30e25214747a12380b
Ville Strengell <villes-97@hotmail.com>
2022-11-07 11:23:27 +0200

    2.5: refactored components to not be inside each other

--- part2/2.kurssitiedot/src/components/Course.js
+++ part2/2.kurssitiedot/src/components/Course.js
@@ -1,42 +1,43 @@
-const Course = ({ course }) => {
-    const Header = ({ coursename }) => {
-      return  <h2> {coursename} </h2>
-    }
-    const Content = ({ parts }) => {
-      const Part = (props) => {
-        return (
-          <>
-            <p>{props.name + " " + props.exercises} </p>
-          </>
-        )
-      }
-      return (
-        <div>
-          {parts.map(
-            part =>
-              <Part key={part.id} name={part.name} exercises={part.exercises} />
-          )}
-        </div>
-      )
-    }
-    const Total = ({ parts }) => {
-      const sum = parts.reduce(
-        (s, p) => s + p.exercises, 0
-      );
-  
-      return (
-        <div>
-          <b>total of {sum} exercises</b>
-        </div>
-      )
-    }
-    return (
-      <div>
-        <Header coursename={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-    )
-  }
+const Header = ({ coursename }) => {
+  return  <h2> {coursename} </h2>
+}
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.name + " " + props.exercises} </p>
+    </>
+  )
+}
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(
+        part =>
+          <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+const Total = ({ parts }) => {
+  const sum = parts.reduce(
+    (s, p) => s + p.exercises, 0
+  );
 
-  export default Course
+  return (
+    <div>
+      <b>total of {sum} exercises</b>
+    </div>
+  )
+}
+const Course = ({ course }) => {
+    
+  return (
+    <div>
+      <Header coursename={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
