31de51f19f9d423263195593c784c7ea182d023f
Ville Strengell <villes-97@hotmail.com>
2022-11-18 15:10:30 +0200

    part 2.13* done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,48 @@
-
-
-
-
-
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const ListOfCountries = ({ countries }) => {
+  // Create array that is same length as "countries", fill it with false's
+  const [infosToShow, setInfosToShow] = useState(
+    new Array(countries.length).fill(false)
+  )
+
+  // When clicked, toggle the state of the boolean in that index
+  const handleShowClick = (index) => {
+    const newInfosToShow = [...infosToShow]
+    newInfosToShow[index] = !newInfosToShow[index]
+    setInfosToShow(newInfosToShow)
+  }
+    
+  return (
+    <div className="countries">
+      {countries.map(
+        (country, idx) => 
+          <div key={country.cca2} className={"country " + country.cca2}>
+            <div>
+              {country.name.common}
+              <input 
+                type="button"
+                className="button" 
+                value="show" 
+                style={{ marginLeft:"5px"}}
+                onClick={() => handleShowClick(idx)}
+              ></input> 
+            </div> 
+
+            <div className={"info"}
+                style={{display: infosToShow[idx] ? 'block' : 'none'}}>
+              <Country country={country}/>
+            </div>
+          </div>
+
+      )}
+    </div>  
+  )
+}
+
 const Countries = ({ countriesToShow }) => {
+  
   if (countriesToShow.length > 10) {
     return ( 
       <div>
@@ -14,14 +50,8 @@ const Countries = ({ countriesToShow }) => {
       </div>  )
   } else if (countriesToShow.length >= 2) {
     return (
-      <div className="countries">
-        {countriesToShow.map(
-          country => 
-            <div key={country.cca2}>
-              {country.name.common}
-            </div>
-        )}
-      </div>  )
+        <ListOfCountries countries={countriesToShow}/>
+    )
   } else if (countriesToShow.length === 1) {
     return (
       <div >
@@ -48,20 +78,19 @@ const Country = ({ country }) => {
       </p>
       <h3>Languages:</h3>
       {languages.map(
-          lang => 
-            <li key={lang}>
-              {lang}
-            </li>
-        )}
+        lang => 
+          <li key={lang}>
+            {lang}
+          </li>
+      )}
       
       <div style={{ border: "solid", 
-                    margin:"5px", 
-                    width:"fit-content" 
+                    margin: "5px", 
+                    width: "fit-content" 
                     }}>
-        <img src={country.flags.png} alt="flag" 
-                  style={{ 
-                    margin:"0px 0px -4px 0px"
-                  }}/>
+        <img src={country.flags.png} 
+            alt="flag" 
+            style={{ margin:"0px 0px -4px 0px" }}/>
       </div>
     </>
   )
