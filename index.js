9cf2cd2fae27b8b43d50c3680bd2b6facdb21eb3
Ville Strengell <villes-97@hotmail.com>
2023-03-28 13:34:03 +0300

    3.2 phonebook infopage added

--- index.js
+++ index.js
@@ -6,36 +6,49 @@ app.use(express.json())
 
 let phonebook = [
     {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
     },
     {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
     },
     {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
     },
     {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
   ]
 
+// Homepage
 app.get('/', (req, res) => {
-  res.send('<h2>Phonebook at /api/phonebook!</h2>')
+    res.send('<h2>Phonebook!</h2>')
+  })
+
+// info page
+app.get('/info', (req, res) => {
+    const now = new Date();
+    const html = `
+        <p>Phonebook has info for ${phonebook.length} people</p>\n
+        <p>${now}</p>
+    `
+    res.send(html)
 })
 
+// API-page
 app.get('/api/phonebook', (req, res) => {
-  res.json(phonebook)
+    res.json(phonebook)
 })
 
+// Start the server
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
