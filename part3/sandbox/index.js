2b41c90322900f3a037b472b08b132ee20e877f3
Ville Strengell <villes-97@hotmail.com>
2023-03-28 11:58:10 +0300

    backend beginning

--- part3/sandbox/index.js
+++ part3/sandbox/index.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -21,11 +23,51 @@ let notes = [
     }
 ]
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h2>Hello World?</h2>')
 })
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end() 
+  }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
