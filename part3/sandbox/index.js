7200fae89fe5a076552804b31d7e998de651a420
Ville Strengell <villes-97@hotmail.com>
2023-05-08 10:02:12 +0300

    3.7-3.8* morgan

--- part3/sandbox/index.js
+++ part3/sandbox/index.js
@@ -3,7 +3,20 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
     {
@@ -69,6 +82,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
