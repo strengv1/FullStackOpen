7a576508d57ccf903f0496d92abdb93ec0c72588
Ville Strengell <villes-97@hotmail.com>
2023-05-23 12:59:59 +0300

    3.17* editing numbers

--- index.js
+++ index.js
@@ -72,6 +72,20 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+// Edit existing person
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+  
 // Delete person
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
