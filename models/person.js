14cd7664c11b8e320c9411342cde5fca9d1ecefb
Ville Strengell <villes-97@hotmail.com>
2023-05-29 12:49:45 +0300

    eslint installation and fixes

--- models/person.js
+++ models/person.js
@@ -7,33 +7,33 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
-})
+  })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlength: 3,
-      required: true
-    },
-    number: {
-      type: String,
-      validate: {
-        validator: num => {
-          const parts = num.split("-")
-          return (num.length >= 8) &&
-                 (parts.length === 2) && 
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: num => {
+        const parts = num.split('-')
+        return (num.length >= 8) &&
+                 (parts.length === 2) &&
                  (parts[0].length === 2 || parts[0].length === 3)
-        },
-        message: props => `${props.value} is not formatted correctly`
-      }
-    },
+      },
+      message: props => `${props.value} is not formatted correctly`
+    }
+  },
 })
-  
+
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
