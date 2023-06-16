b00c2c33c62fb3c48d418517af4124c66c3e767d
Ville Strengell <villes-97@hotmail.com>
2023-06-16 13:24:10 +0300

    4.6* and 4.7* added functions mostBlogs and mostLikes + their tests

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+var lodash = require('lodash')
+
 const dummy = () => {
   // ...
   return 1
@@ -15,8 +17,44 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce( (prev, current) => prev.likes > current.likes ? prev : current, {} )
 }
 
+const mostBlogs = (blogs) => {
+  // reduce the array "blogs" to an object containing all authors,
+  // containing all their blogs
+  const authorObj = lodash.groupBy(blogs, 'author')
+
+  // Find author with mosts blogs
+  const authorWithMostBlogs = lodash
+    .maxBy(Object.keys(authorObj), (o) => authorObj[o].length)
+
+  return authorWithMostBlogs ? {
+    author: authorWithMostBlogs,
+    blogs: authorObj[authorWithMostBlogs].length
+  } : {}
+}
+
+const mostLikes = (blogs) => {
+  // Reduce the array "blogs" to object containing all authors
+
+  const authorObj = blogs.reduce((authors, current) => {
+    authors[current.author] = authors[current.author] + current.likes || current.likes
+    return authors
+  }, {})
+
+  // Find author with most likes
+  const authorWithMostLikes = lodash.
+    maxBy(Object.keys(authorObj), (o) => authorObj[o])
+
+  return {
+    author: authorWithMostLikes,
+    likes: authorObj[authorWithMostLikes]
+  }
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
