4d56b083c5204ad7df0e2d884475c431920c83c6
Ville Strengell <villes-97@hotmail.com>
2023-06-27 13:29:43 +0300

    4.9 blog api tests for get

---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,39 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Esimerkkiblogi',
+    author: 'Kalle Kirjailija',
+    url: 'url.com',
+    likes: 1,
+  },
+  {
+    title: 'Miksi testaaminen on helppoa ja kivaa',
+    author: 'Teemu Testeri',
+    url: 'testisivu.com',
+    likes: 13
+  },
+  {
+    title: 'Miksi node on helppoa ja kivaa',
+    author: 'Kalle Koodari',
+    url: 'hieno-osoite.com',
+    likes: 4
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({})
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
