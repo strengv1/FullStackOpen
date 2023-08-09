b90c3be849316732e8814bc244a27a19c608de9f
Ville Strengell <villes-97@hotmail.com>
2023-08-09 13:27:04 +0300

    4.21* require correct user token when deleting a blog

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -43,29 +43,44 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
-async function deleteBlogFromUserDb(blog) {
-  const user = await User.findById(blog.user)
+async function deleteBlogFromUserDb(blog, user) {
   if (user){
     user.blogs = user.blogs.filter(b => b.toString() !== blog.id)
     await user.save()
   }
 }
+async function getUserAndAuthenticate(blog, reqToken) {
+  const userFromBlog = await User.findById(blog.user)
+  const userFromToken = jwt.verify(reqToken, process.env.SECRET)
+  const auth = userFromToken.id === userFromBlog._id.toString()
 
-blogsRouter.delete('/:id', async (request, response) => {
+  console.log(userFromBlog)
+  console.log(userFromToken.id, '===', userFromBlog._id.toString())
+
+  return { userFromBlog, auth }
+}
+
+blogsRouter.delete('/:id', async (request, response, next) => {
   // Check for a valid mongoose _id. NOTE: Does not check if id exists in database
   if (!isValidObjectId(request.params.id)) {
-    return response.status(404).json({ error: 'Invalid id' }).end()
+    return response.status(400).json({ error: 'Invalid id' }).end()
   }
   try {
     const blog = await Blog.findById(request.params.id)
-    if (blog){
-      await Blog.deleteOne(blog)
-      deleteBlogFromUserDb(blog)
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
     }
 
-    response.status(204).end()
+    const { user, auth } = await getUserAndAuthenticate(blog, request.token)
+    if (auth){
+      await Blog.deleteOne(blog)
+      await deleteBlogFromUserDb(blog, user)
+      response.status(204).end()
+    } else {
+      response.status(401).json({ error: 'authentication failed' }).end()
+    }
   } catch(err){
-    return response.status(400).json({ error: err.message })
+    next(err)
   }
 })
 
