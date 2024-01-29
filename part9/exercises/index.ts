ca1e007dd433aa03cf5aa5ad4f12c7c60d6d9e7a
Ville Strengell <villes-97@hotmail.com>
2024-01-29 13:05:00 +0200

    9.4-9.5

---
+++ part9/exercises/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { calculateBmi } from './bmiCalculator'; 
+
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+    res.status(400).json({ error: 'malformatted parameters' });
+    return;
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  res.json({ height, weight, bmi });
+})
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
